a23646cc593aed0bc13ad2b6f302ad0eccc061e6
vijayakumar58 <vijayalingam58@gmail.com>
2022-11-19 22:54:53 +0530

    add js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ weather.then((data)=>data.json()).then((res)=>{console.log(res)});
 
 async function foo(){
    const weatherApi=await fetch("https://api.openweathermap.org/data/2.5/weather?q=chennai&appid=87fe926676d0f2ab9b9cda9b1587fb87")
-   console.log(weatherApi);
+   // console.log(weatherApi);
    const res=await weatherApi.json()
    console.log(res);
 }
